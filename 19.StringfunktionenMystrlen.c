cc8629915400ccd3ce330bc435ba9b281831c478
Nevena <nevena.rogova@gmx.at>
2025-03-23 20:24:41 +0200

    Update 19.StringfunktionenMystrlen.c

--- 19.StringfunktionenMystrlen.c
+++ 19.StringfunktionenMystrlen.c
@@ -1,23 +1,26 @@
+
+
 /* strlen.c */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 int mystrlen(char zeile[])
 {
-	int i = 0;
-	for (i = 0; i < 80 && zeile[i] != '\0';i++) {};
-	return i;
-
+    int i = 0;
+    for (i = 0; i < 80 && zeile[i] != '\0'; i++) {};
+    return i;
 }
 
 int main(void) {
-	char string[] = "Das ist ein Teststring";
-	int laenge;
-
-	laenge = strlen(string);
-	printf("Der String \"%s\" hat %d Zeichen\n", string, laenge);
-
-	laenge = mystrlen(string);
-	printf("Der myString \"%s\" hat %d Zeichen\n", string, laenge);
-
-	return 0;
+    char string[] = "Das ist ein Teststring";
+    int laenge;
+    
+    laenge = strlen(string);
+    printf("Der String \"%s\" hat %d Zeichen\n", string, laenge);
+    
+    laenge = mystrlen(string);
+    printf("Der myString \"%s\" hat %d Zeichen\n", string, laenge);
+    
+    return 0;
+}
