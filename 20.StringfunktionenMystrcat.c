edc1cdf8cd35fc156db56534b6dd52c9864d69ea
Nevena <nevena.rogova@gmx.at>
2025-03-23 20:21:05 +0200

    Update 20.StringfunktionenMystrcat.c

--- 20.StringfunktionenMystrcat.c
+++ 20.StringfunktionenMystrcat.c
@@ -1,20 +1,30 @@
-// 20.StringfunktionenMystrcat.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* stringcopy.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <iostream>
-
-int main()
+int mystrcpy(char s1[], char s2[])
 {
-    std::cout << "Hello World!\n";
+	int i = 0, pos;
+	pos = strlen(s2);
+
+	for (i = 0; i <= strlen(s2);i++)  //Stringende '\0' mitkopieren
+	{
+		s1[i] = s2[i];
+	}
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main(void) {
+	char quelle[80] = "Dieser Text wurde kopiert ";
+	char kopie[80];
+
+	strcpy(kopie, quelle);
+	printf("Ergebnis strcpy:   %s\n", kopie);
+
+	mystrcpy(kopie, quelle);
+	printf("Ergebnis mystrcpy: %s\n", kopie);
+
+	return 0;
+}
