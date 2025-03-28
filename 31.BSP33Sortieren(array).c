d82f539236535d51a4322e4cc96882709b5dc430
Nevena <nevena.rogova@gmx.at>
2025-03-28 23:18:53 +0200

    Add files via upload

---
+++ 31.BSP33Sortieren(array).c
@@ -0,0 +1,21 @@
+//BSP33:Sortieren(array)
+
+
+
+void bubblesort(int array[], int length)
+{
+    int i, j, tmp;
+
+    for (i = 1; i < length; i++)
+    {
+        for (j = 0; j < length - 1; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+}
