009bf4f428362eef1e77c33186eed38b67ad99f6
Nevena <nevena.rogova@gmx.at>
2025-05-25 12:10:00 +0300

    Update 38.StacksMittelsVerketteterListeLIFO.c

--- 38.StacksMittelsVerketteterListeLIFO.c
+++ 38.StacksMittelsVerketteterListeLIFO.c
@@ -1,20 +1,92 @@
-// 38.StacksMittelsVerketteterListeLIFO.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
-#include <iostream>
+struct node {
+    int data;
+    struct node* next;
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+struct node* front = NULL;
+
+void push(int y) {
+    struct node* x = (struct node*)malloc(sizeof(struct node));
+    if (x == NULL) {
+        printf("Fehler beim Speicher reservieren!\n");
+        exit(1);
+    }
+    x->data = y;
+    x->next = front;
+    front = x;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void display() {
+    struct node* temp = front;
+    while (temp != NULL) {
+        printf("Wert: %d\n", temp->data);
+        temp = temp->next;
+    }
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void clear_stack() {
+    struct node* temp = front;
+    while (temp != NULL) {
+        struct node* help = temp;
+        temp = temp->next;
+        free(help);
+    }
+    front = NULL;
+}
+
+int pop() {
+    if (front == NULL) {
+        printf("STACK EMPTY...\n");
+        return -1;
+    }
+
+    int value = front->data;
+    struct node* temp = front;
+    front = front->next;
+    free(temp);
+    return value;
+}
+
+int stack_empty() {
+    return front == NULL;
+}
+
+int main() {
+    int num;
+
+    printf("Testprogramm: Stack mittels verketteter Liste\n\n");
+
+    num = 1;
+    push(num);
+    printf("push(): Wert %d wurde gespeichert\n", num);
+
+    num = 3;
+    push(num);
+    printf("push(): Wert %d wurde gespeichert\n", num);
+
+    num = 5;
+    push(num);
+    printf("push(): Wert %d wurde gespeichert\n", num);
+
+    printf("\nDer Stack beinhaltet folgende Elemente:\n");
+    display();
+
+    printf("\npop(): Die Funktion pop() liefert den Wert: %d\n", pop());
+
+    printf("\nDer Stack beinhaltet folgende Elemente:\n");
+    display();
+
+    clear_stack();
+    printf("\nDer gesamte Stack wurde geleert.\n");
+
+    if (stack_empty()) {
+        printf("\n\t\tSTACK EMPTY\t\t\n");
+    } else {
+        printf("\n\t\tSTACK NOT EMPTY\t\t\n");
+    }
+
+    return 0;
+}
